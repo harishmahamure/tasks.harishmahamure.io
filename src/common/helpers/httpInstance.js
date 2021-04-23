8f770c839c078c59f3aaf4ba122e8dd9ab31aa22
Harish <hmahamure@gmail.com>
2021-04-23 15:40:45 +0530

    key

--- src/common/helpers/httpInstance.js
+++ src/common/helpers/httpInstance.js
@@ -3,26 +3,26 @@ import { BASE_URL } from "../constants/APIEndpoints";
 require("dotenv").config();
 
 const httpInstance = axios.create({
-	baseURL: BASE_URL,
+    baseURL: BASE_URL,
 });
 
 httpInstance.interceptors.request.use(
-	(apiConfig) => {
-		apiConfig.headers["AuthToken"] = "0ruAaKiEo46PUYqMxFHH895DWivpeVrZ";
-		return { ...apiConfig };
-	},
-	(err) => {
-		return Promise.reject(err);
-	}
+    (apiConfig) => {
+        apiConfig.headers["AuthToken"] = "xvF43K6mRV4xc6OZnfMbSfq5Y73EydD9";
+        return { ...apiConfig };
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
 );
 
 httpInstance.interceptors.response.use(
-	(res) => {
-		return res.data;
-	},
-	(err) => {
-		return err;
-	}
+    (res) => {
+        return res.data;
+    },
+    (err) => {
+        return err;
+    }
 );
 
 export default httpInstance;
